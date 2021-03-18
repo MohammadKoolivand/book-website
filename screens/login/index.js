88486623427b208f7ab4bff86e60332d978aa2cd
Mohammad <48600357+MohammadKoolivand@users.noreply.github.com>
2021-03-18 21:55:26 +0330

    day-001.1

--- screens/login/index.js
+++ screens/login/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
-import LoginScreen from "./pages/SigninScreen";
 import RegisterScreen from "./pages/SignupScreen";
+import LoginScreen from "./pages/SigninScreen";
 
 const Stack = createStackNavigator()
 export default function LoginNavigator({ setIsLogin }) {
